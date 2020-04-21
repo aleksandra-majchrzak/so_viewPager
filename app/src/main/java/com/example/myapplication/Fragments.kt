b88e9314901087def1fd8cf8f362f22f5aab6cb1
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2020-04-21 16:29:46 +0200

    Update

--- app/src/main/java/com/example/myapplication/Fragments.kt
+++ app/src/main/java/com/example/myapplication/Fragments.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_profile.*
@@ -13,15 +15,13 @@ class ProfileFragment : Fragment() {
 
     private lateinit var tabsText: List<String>
     private val adapter by lazy {
-        ViewPagerAdapter(this)
+        ViewPagerAdapter(childFragmentManager, lifecycle)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        tabsText = listOf<String>(
-            getString(R.string.tab1),
-            getString(R.string.tab2), getString(R.string.tab3)
-        )
+        tabsText = listOf<String>(getString(R.string.tab1),
+            getString(R.string.tab2), getString(R.string.tab3))
     }
 
     override fun onCreateView(
@@ -34,8 +34,6 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
         pager.adapter = adapter
 
         TabLayoutMediator(tabs, pager) { tab, position ->
@@ -44,10 +42,10 @@ class ProfileFragment : Fragment() {
     }
 }
 
-class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+class ViewPagerAdapter(manager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(manager, lifecycle) {
     override fun getItemCount(): Int = 3
 
-    override fun createFragment(position: Int): Fragment = when (position) {
+    override fun createFragment(position: Int): Fragment = when(position) {
         0 -> UserInfoFragment.newInstance()
         1 -> SavedRecipesFragment.newInstance()
         else -> ShoppingListFragment.newInstance()
